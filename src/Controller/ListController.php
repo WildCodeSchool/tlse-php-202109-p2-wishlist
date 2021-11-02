1c0a9da596ecaf0af228f12f1ddc3f0439657dbd
Asa <danielbatalhagoncalves.dev@gmail.com>
2021-11-02 15:31:03 +0100

    page list/share route + controller pour travailler la view. View css ok. En attente de la navabar desktop

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -2,16 +2,31 @@
 
 namespace App\Controller;
 
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
 class ListController extends AbstractController
 {
     /**
      * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
     public function index(): string
     {
         return $this->twig->render('List/index.html.twig');
+    }
+
+    /**
+     * @return string
+     * @throws RuntimeError
+     * @throws SyntaxError
+     * @throws LoaderError
+     */
+    public function share(): string
+    {
+        return $this->twig->render('List/share.html.twig');
     }
 }
