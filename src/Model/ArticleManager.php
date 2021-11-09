80a7d445b287a29422dce7d958a686e75e45ca7a
SANCHEZ nicolas <77881361+nicopierresanchez@users.noreply.github.com>
2021-11-09 17:24:41 +0100

    modification query items

--- src/Model/ArticleManager.php
+++ src/Model/ArticleManager.php
@@ -23,13 +23,14 @@ class ArticleManager extends AbstractManager
     public function insertArticle(array $article)
     {
         $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE .
-        " ('name', 'market_link','picture','price','list_id') 
-        VALUES (:name, :market_link, :picture, :price, :list_id, :market_link)");
-        $statement->bindValue('title', $article['title'], \PDO::PARAM_STR);
-        $statement->bindValue('market_link', $article['market_link'], \PDO::PARAM_STR);
-        $statement->bindValue('picture', $article['picture'], \PDO::PARAM_STR);
-        $statement->bindValue('price', $article['price'], \PDO::PARAM_INT);
-        $statement->bindValue('list_id', $article['list_id']);
+        " (`name`, `market_link`,`picture`,`description`,`price`,`list_id`) 
+        VALUES (:name, :market_link, :picture, :description, :price, :list_id, :market_link)");
+        $statement->bindValue(':name', $article['name'], \PDO::PARAM_STR);
+        $statement->bindValue(':market_link', $article['market_link'], \PDO::PARAM_STR);
+        $statement->bindValue(':picture', $article['picture'], \PDO::PARAM_STR);
+        $statement->bindValue(':description', $article['description'], \PDO::PARAM_STR);
+        $statement->bindValue(':price', $article['price'], \PDO::PARAM_INT);
+        $statement->bindValue(':list_id', $article['list_id']);
 
         $statement->execute();
         return (int)$this->pdo->lastInsertId();
