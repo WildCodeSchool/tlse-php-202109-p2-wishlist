af1d6d1121f2c2c23ae0044d366144fabdbcf540
Loic Fremond <loic.fremond@hotmail.fr>
2021-11-15 17:52:18 +0100

    update css

---
+++ public/assets/javascript/add-article.js
@@ -0,0 +1,7 @@
+const add = document.querySelector(".add");
+const addArticle = document.querySelector(".form");
+
+add.addEventListener("click", function() {
+console.log("test");
+addArticle.classList.toggle("addArticle-visible");
+});
