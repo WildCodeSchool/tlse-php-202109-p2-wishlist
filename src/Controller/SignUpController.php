3f38aacf081f6bf3bbd8152e2607a0605276e930
Asa <danielbatalhagoncalves.dev@gmail.com>
2021-11-18 12:13:09 +0100

    hotfix css and controller create account

--- src/Controller/SignUpController.php
+++ src/Controller/SignUpController.php
@@ -9,8 +9,15 @@ class SignUpController extends AbstractController
 
     public function index(): string
     {
-        $userConnection = new UserManager();
+        if (isset($_SESSION['user']) && isset($_SESSION['session_id'])) {
+            header('Location: /list/user/lists');
+        }
+        if (session_status() !== PHP_SESSION_NONE) {
+            session_regenerate_id(true);
+            $_COOKIE['PHPSESSID'] = session_id();
+        }
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $userConnection = new UserManager();
             $errors = $userConnection->errorsInForm($_POST);
 
             if (!empty($errors)) {
@@ -23,7 +30,7 @@ class SignUpController extends AbstractController
             $sessionId = session_id();
             $_POST['session'] = $sessionId;
             $userConnection->createUser($_POST);
-            return $this->twig->render('List/lists.html.twig');
+            header('Location: /login');
         }
         return $this->twig->render('Signup/index.html.twig');
     }
